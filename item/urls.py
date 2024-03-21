64d5572e050a7a5ceaef82465635b2257e5b5d8c
rono516 <ronocollins2000@gmail.com>
2024-03-21 15:58:12 +0300

    fix/no-ref/add-dashboard

--- item/urls.py
+++ item/urls.py
@@ -4,6 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 app_name = "item"
-urlpatterns = [path("<int:pk>/", views.detail, name="detail")] + static(
-    settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
-)
+urlpatterns = [
+    path("new/", views.new, name="new"),
+    path("<int:pk>/", views.detail, name="detail"),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
